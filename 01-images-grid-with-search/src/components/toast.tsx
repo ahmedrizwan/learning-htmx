961753f8aad3c0a771c98cf8157795901f131f36
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2024-02-18 04:21:34 +0500

    feat: randomly fail favorite action

--- 01-images-grid-with-search/src/components/toast.tsx
+++ 01-images-grid-with-search/src/components/toast.tsx
@@ -2,7 +2,7 @@ export async function Toast(props: { text: string }) {
   return (
     <div
       id={`toast`}
-      class={`absolute left-16 bottom-6 sticky`}
+      class={`sticky left-16 bottom-6`}
       hx-swap-oob="true"
     >
       <div class="inline-block pr-4 pl-4 pt-2 pb-2 rounded-md bg-white shadow-md hide-animation">
--- 01-images-grid-with-search/src/index.tsx
+++ 01-images-grid-with-search/src/index.tsx
@@ -82,6 +82,31 @@ app.put("/favorite", async (c) => {
     const image = images[index];
     image.favorited = !image.favorited;
 
+    // Randomly fail this action
+    if (Math.round(Math.random()) === 0) {
+      return c.html(
+        <>
+          <svg
+            id={`heart-${id}`}
+            hx-swap-oob="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill={!image.favorited ? "red" : "white"}
+            class="bi bi-heart-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+            />
+          </svg>
+
+          <Toast text={`Action failed, please try again!`} />
+        </>
+      );
+    }
+
     return c.html(
       <svg
         id={`heart-${id}`}
