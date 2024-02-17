95531a121aac2c732a7955f934c128927b3cf482
Ahmed Rizwan <ahmed.rizwan@epicgames.quixel.com>
2024-02-18 01:44:07 +0500

    feat: load page on initial load

--- 01-images-grid-with-search/src/components/grid.tsx
+++ 01-images-grid-with-search/src/components/grid.tsx
@@ -1,8 +1,17 @@
 export function Grid() {
   return (
-    <div
-      id="results-grid"
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
-    ></div>
+    <>
+      <div
+        hx-get="/images?page=1"
+        hx-trigger="load"
+        hx-target="#results-grid"
+        hx-swap="innerHTML"
+        hx-indicator="#spinner"
+      ></div>
+      <div
+        id="results-grid"
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
+      ></div>
+    </>
   );
 }
